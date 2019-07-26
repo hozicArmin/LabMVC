16e37271f1f3f6d9e9be9bf157cbb8e19a93deb2
Armin Hozic <hozicArmin@users.noreply.github.com>
2019-07-26 22:19:56 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabMVC.Startup))]
+namespace LabMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
